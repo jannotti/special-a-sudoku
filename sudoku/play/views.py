63c653b45d7660fc5771363d019d720b34b72e57
John Jannotti <jj@cs.brown.edu>
2018-07-24 13:45:12 +0800

    add entire jj.html as a temple, use a variable

--- sudoku/play/views.py
+++ sudoku/play/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
   return HttpResponse("This could list playable boards.")
 
 def show(request, board_id):
   return HttpResponse("This would show board {}.".format(board_id))
+
+def sample(request):
+    template = loader.get_template('sample.html')
+    context = {
+        'leading_text': "This could be anything",
+    }
+    return HttpResponse(template.render(context, request))
